56ae7840447bc635622ed4f1972b27598785a631
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-04-13 18:46:46 -0500

    Se realizó el Ejemplo2

---
+++ weekly_mission_1/example2/logger.js
@@ -0,0 +1,9 @@
+// Función que se guardará en este módulo como información 
+exports.info = (message) => {
+    console.log(`info: ${message}`);
+}
+
+//Función que se guardará en este módulo como verbose
+exports.verbose = (message) => {
+    console.log(`verbose: ${message}`);
+}
