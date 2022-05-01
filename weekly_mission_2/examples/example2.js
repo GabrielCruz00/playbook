1600dd57b2835238331a08005d9c9fd7f07c388d
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-04-30 22:31:17 -0500

    Se termina Ejemplo 2: Objeto con propiedades

---
+++ weekly_mission_2/examples/example2.js
@@ -0,0 +1,14 @@
+console.log('Ejemplo 2: Crear un objeto con propiedades');
+console.log('');
+
+const myObject = {
+    name: 'Gabriel',
+    age: '22'
+}
+
+console.log(`"const myObject = {
+    name: 'Gabriel',
+    age: '22'
+}"`);
+console.log('');
+console.log(myObject);
