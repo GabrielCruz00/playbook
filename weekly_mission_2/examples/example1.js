bc0c73b2f013922fb181535ef008ed88de36d20c
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-04-30 22:29:50 -0500

    Se agrega console.log con titulo del ejemplo

--- weekly_mission_2/examples/example1.js
+++ weekly_mission_2/examples/example1.js
@@ -1,5 +1,8 @@
-// Ejemplo 1: Crear un objeto
+console.log('Ejemplo 1: Crear un objeto vacio');
+console.log('');
+
 const myObject = {}
 
 console.log('const myObject = {}');
+console.log('');
 console.log(myObject);
