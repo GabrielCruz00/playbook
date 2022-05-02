92a6542f3fd0b3e9e85c968458eb12397e8fb739
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-05-01 22:07:41 -0500

    Se termina Ejemplo 3: forEach + toUpperCase

--- weekly_mission_2/examples/2.-Operators/example3.js
+++ weekly_mission_2/examples/2.-Operators/example3.js
@@ -1,14 +1,4 @@
-console.log('Ejemplo 4: Uso de map para recorrer los elementos de una lista');
-const numbers = [1, 2, 3, 4, 5];
-const numbersSquare = numbers.map(function(num){
-    return num * num;
-});
-console.log('');
+console.log('Ejemplo 3: forEach para imprimir los paises en letras mayusculas');
 
-console.log('const numbersSquare = numbers.map(function(num){return num * num;});')
-console.log(numbersSquare);
-console.log('');
-
-console.log('const numbersSquare2 = numbers.map((num) => {return num * num});')
-const numbersSquare2 = numbers.map((num) => {return num * num});
-console.log(numbersSquare2);
+const countries = ['Findland', 'Denmark', 'Sweden', 'Norway', 'Iceland'];
+countries.forEach( countrie => console.log(countrie.toUpperCase()) );
