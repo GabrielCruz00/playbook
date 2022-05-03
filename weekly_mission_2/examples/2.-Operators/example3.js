e74d14d51b93bc289a6498634e72c7912ccf98dd
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-05-03 15:00:27 -0500

    Se corrige error de grámatica en example3.js

--- weekly_mission_2/examples/2.-Operators/example3.js
+++ weekly_mission_2/examples/2.-Operators/example3.js
@@ -1,4 +1,4 @@
 console.log('Ejemplo 3: forEach para imprimir los paises en letras mayusculas');
 
 const countries = ['Findland', 'Denmark', 'Sweden', 'Norway', 'Iceland'];
-countries.forEach( countrie => console.log(countrie.toUpperCase()) );
+countries.forEach( country => console.log(country.toUpperCase()) );
