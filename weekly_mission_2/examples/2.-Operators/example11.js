164e45760bc80fde79c654164d24d9bec6360823
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-05-03 19:42:14 -0500

    Se termina ejemplo 11: Uso de find para encontrar el primer elemento que cumpla con determinada característica

---
+++ weekly_mission_2/examples/2.-Operators/example11.js
@@ -0,0 +1,6 @@
+console.log('Ejemplo 11: Uso de find para encontrar el primer elemento de una lista que cumpla con lo que se indique');
+
+const ages = [24, 22, 19, 25, 32, 35, 18];
+
+const age = ages.find( age => age < 20);
+console.log('Primer edad menor a 20 es: ' + age);
