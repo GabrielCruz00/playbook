95246a25cf9bb0c402d4bbcf170625e13e35a2e4
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-05-03 22:42:54 -0500

    Se termina Ejemplo 2: Un objeto a partir de una clase

---
+++ weekly_mission_2/examples/3.-ClasesyObjetos/example2.js
@@ -0,0 +1,9 @@
+console.log('Ejemplo 2: Crear un objeto a partir de una clase');
+
+class pet {
+
+};
+
+const myPet1 = new pet(); // la clase pet es el molde, y puedo crear muchos objetos a partir del molde
+
+console.log(myPet1);
