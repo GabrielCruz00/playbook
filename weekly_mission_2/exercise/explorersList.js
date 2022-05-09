84b91140e749c47d6f3804e6f20678f9b5608b20
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-05-09 16:50:42 -0500

    Se crea la lista de explorers para el Ejercicio 2

---
+++ weekly_mission_2/exercise/explorersList.js
@@ -0,0 +1,56 @@
+export const explorers = [
+    {
+        name: 'Explorer 1',
+        exercisesCompleted: 10,
+        rate: 99,
+        city: 'CDMX',
+        stack: ['js', 'C#'],
+        missions: {
+            onboarding: {
+                isFinished: true,
+                excercisesFinished: true
+            },
+
+            frontend: {
+                isFinished: true,
+                exercisesCompleted: true
+            }
+        }
+    },
+    {
+        name: 'Explorer 2',
+        exercises_completed: 9,
+        city: 'Veracruz',
+        rate: 50,
+        stack: ['js'],
+        missions: {
+          onboarding: {
+            isFinished: false,
+            exercisesFinished: false
+          },
+
+          frontend: {
+            isFinished: false,
+            exercisesFinished: false
+          }
+        }
+      },
+      {
+        name: 'Explorer 3',
+        exercises_completed: 3,
+        city: 'Sonora',
+        rate: 100,
+        stack: ['elixir'],
+        missions: {
+          onboarding: {
+            isFinished: true,
+            exercisesFinished: true
+          },
+          
+          frontend: {
+            isFinished: false,
+            exercisesFinished: false
+          }
+        }
+      }
+]
