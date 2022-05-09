d82eb5b00c90ced9d3b19126f40d7d14e80eaba8
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-05-08 20:49:34 -0500

    Se corrige linea de codigo de la prueba unitaria. Resultado: Pass

--- weekly_mission_2/examples/5.-UnitTesting/pokemon.test.js
+++ weekly_mission_2/examples/5.-UnitTesting/pokemon.test.js
@@ -2,5 +2,7 @@ import Pokemon from './pokemon'
 
 test('1) Create a new object pokemon', () => {
   const myPokemon = new Pokemon('Pikachu')
-  expect(myPokemon.name).toBe('Pikachussssssss'); // Corrige esta prueba
+  // expect(myPokemon.name).toBe('Pikachussssssss'); // Corrige esta prueba
+  // la linea anterior arroja un error pues el nombre del pokemon no es Pikachussss
+  expect(myPokemon.name).toBe('Pikachu');
 });
