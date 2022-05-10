b5d04d349071225f89a9c3ae52b05a5207db2fa4
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-05-09 20:03:48 -0500

    4) Lista de explorers con stack: js

--- weekly_mission_2/exercise/exercise2.js
+++ weekly_mission_2/exercise/exercise2.js
@@ -9,3 +9,7 @@ explorers.forEach(explorer => console.log(`${explorer.name}: ${explorer.stack}`)
 console.log('3) Creando una nueva lista de stacks');
 const stacks = explorers.map(item => item.stack);
 console.log(stacks);
+
+console.log('4) Lista de explorers con stack: js');
+const stackJS = explorers.filter(item => item.stack.includes('js'));
+console.log(stackJS);
