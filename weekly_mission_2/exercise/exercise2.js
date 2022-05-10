b901898e75c5289dc47d68d00e534b61eb264ed0
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-05-09 19:46:26 -0500

    2) Imprimiendo el Stack de cada explorer

--- weekly_mission_2/exercise/exercise2.js
+++ weekly_mission_2/exercise/exercise2.js
@@ -1,4 +1,7 @@
 import {explorers} from './explorersList.js';
 
-console.log('19 Imprimiendo el nombre de cada explorer');
+console.log('1) Imprimiendo el nombre de cada explorer');
 explorers.forEach( explorer => console.log(explorer.name));
+
+console.log('2) Imprimiendo el stack de cada explorer');
+explorers.forEach(explorer => console.log(`${explorer.name}: ${explorer.stack}`));
