b4ca6888705b1552e14b1c6280b8b9139b7c9c92
GabrielCruz00 <gabrielcruz41@aragon.unam.mx>
2022-05-09 19:49:53 -0500

    3) Creando una nueva lista de stacks

--- weekly_mission_2/exercise/exercise2.js
+++ weekly_mission_2/exercise/exercise2.js
@@ -5,3 +5,7 @@ explorers.forEach( explorer => console.log(explorer.name));
 
 console.log('2) Imprimiendo el stack de cada explorer');
 explorers.forEach(explorer => console.log(`${explorer.name}: ${explorer.stack}`));
+
+console.log('3) Creando una nueva lista de stacks');
+const stacks = explorers.map(item => item.stack);
+console.log(stacks);
